ae61892b997f1b32c3d27558982cf835de382089
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2023-01-19 15:16:00 +0300

    home

---
+++ MainWindow/main.cpp
@@ -0,0 +1,111 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<Windows.h>
+#include<stdio.h>
+
+CONST CHAR gsz_MY_WINDOW_CLASS[] = "MyWindowClass";
+CONST CHAR gsz_WINDOW_NAME[] = "My First Window";
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//1) Регистрация класса окна:
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.style = 0;
+	wc.hIcon = (HICON)LoadImage(hInstance, "fire.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hIconSm = (HICON)LoadImage(hInstance, "fruit.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wc.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	wc.hCursor = (HICON)LoadCursorFromFile("pumpkin.cur");
+	wc.hInstance = hInstance;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = gsz_MY_WINDOW_CLASS;
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	//2) Создание окна:
+
+	int screen_width = GetSystemMetrics(SM_CXSCREEN);
+	int screen_height = GetSystemMetrics(SM_CYSCREEN);
+	int window_width = screen_width * 3 / 4;
+	int window_height = screen_height * 3 / 4;
+	int start_x = screen_width / 8;
+	int start_y = screen_height / 8;
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,    //ExStyle
+		gsz_MY_WINDOW_CLASS,           //Class name
+		gsz_WINDOW_NAME,              //Widows name
+		WS_OVERLAPPEDWINDOW,           //dwStyle
+		CW_USEDEFAULT, CW_USEDEFAULT,  //Начальная позиция окна
+		CW_USEDEFAULT, CW_USEDEFAULT,  //Размер окна
+		NULL,       //HWND родительского окна. У главного окна НЕТ родительского окна
+		NULL,       //Menu отсутствует
+		hInstance,   //
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);  //Задает режим отображения окна (свернуто в окно, развернуто на весь экран)
+	UpdateWindow(hwnd); //Прорисовывает содержимое окна
+
+	//3) Запуск цикла сообщений:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)>0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:break;
+	case WM_COMMAND:break;
+	case WM_SIZE:
+	case WM_MOVE:
+	{
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE] = {};
+		sprintf(sz_buffer, "%s, Position:%dx%d, Size:%dx%d", gsz_WINDOW_NAME,
+			rect.left, rect.top,
+			rect.right - rect.left, rect.bottom - rect.top);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+	}
+	break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	case WM_CLOSE:
+		switch (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Вопрос на миллион долларов", MB_YESNO | MB_ICONQUESTION))
+		{
+		case IDYES:
+			MessageBox(hwnd, "Лучше бы дверь закрыли:)", "Полезная инфа", MB_OK | MB_ICONQUESTION);
+			DestroyWindow(hwnd);
+			break;
+		}
+		break;
+	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
